f80949fedb5d1cd735ade8f329ca275db16c0f87
Michiel Borkent <michielborkent@gmail.com>
2022-03-14 13:47:56 +0100

    Bump SCI

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.borkdude/grasp "0.0.2"
+(defproject io.github.borkdude/grasp "0.0.3"
   :description "Grep Clojure code using clojure.spec regexes."
   :url "https://github.com/borkdude/grasp"
   :scm {:name "git"
@@ -6,7 +6,8 @@
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.babashka/sci "0.3.2"]]
   :plugins [[lein-codox "0.10.7"]
             [lein-cloverage "1.2.2"]]
   :codox {:output-path "gh-pages"}
