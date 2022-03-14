3d7eaa876c325ee73136c504513e109126c2823f
Michiel Borkent <michielborkent@gmail.com>
2022-03-14 13:27:34 +0100

    Add project.clj for deployment

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject io.github.borkdude/grasp "0.0.1"
+  :description "Grep Clojure code using clojure.spec regexes."
+  :url "https://github.com/borkdude/grasp"
+  :scm {:name "git"
+        :url "https://github.com/borkdude/grasp"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"}
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-codox "0.10.7"]
+            [lein-cloverage "1.2.2"]]
+  :codox {:output-path "gh-pages"}
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
